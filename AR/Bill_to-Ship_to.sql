fadb0cca48cc86037a61c87411fc94d8648584fd
Luis Enrique Canseco Gutiérrez <lcanseco.enrique@gmail.com>
2022-06-07 20:11:34 -0500

    Update Bill_to-Ship_to.sql

--- AR/Bill_to-Ship_to.sql
+++ AR/Bill_to-Ship_to.sql
@@ -4,9 +4,9 @@ SELECT hcsu.location                 -- Site number shown in AR transaction page
   FROM hz_cust_accounts        hca
      , hz_cust_acct_sites_all  hcsa
      , hz_cust_site_uses_all   hcsu
- WHERE hca.cust_account_id = hcsa.cust_account_id
+ WHERE hca.cust_account_id    = hcsa.cust_account_id
    AND hcsa.cust_acct_site_id = hcsu.cust_acct_site_id
-   AND HCSU.SITE_USE_CODE = 'BILL_TO';
+   AND HCSU.SITE_USE_CODE     = 'BILL_TO';
 
 --- Ship-To site number:
 
@@ -15,7 +15,7 @@ SELECT hps.party_site_number         -- Ship-To Site number shown in AR transact
      , hz_cust_acct_sites_all  hcsa
      , hz_cust_site_uses_all   hcsu
      , hz_party_sites          hps
- WHERE hca.cust_account_id = hcsa.cust_account_id
+ WHERE hca.cust_account_id    = hcsa.cust_account_id
    AND hcsa.cust_acct_site_id = hcsu.cust_acct_site_id
-   AND hcsa.party_site_id = hps.party_site_id
-   AND HCSU.SITE_USE_CODE = 'SHIP_TO';
+   AND hcsa.party_site_id     = hps.party_site_id
+   AND HCSU.SITE_USE_CODE     = 'SHIP_TO';
